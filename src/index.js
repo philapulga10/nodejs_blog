0a019e6ad6f909fa37db3d28be10a170a77d3b8c
phi_lapulga10 <phinguyenuit10@gmail.com>
2021-04-16 15:08:48 +0700

    study routes and controller

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+// từ version 4.16 trở đi đã tích hợp body-parse
+// middleware để xử lý dữ liệu từ form submit
+app.use(express.urlencoded({ extended: true }))
+// XMLHttpRequest, fetch, axios, ajax (tất cả đều gửi từ js lên server) ==> tất cả đều gửi data lên server mà không phải form
+app.use(express.json())
+
+// *** tại sao parse được body request ==> object ==> vì body parse sử dụng qs
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -17,13 +27,8 @@ app.set('view engine', 'hbs')
 
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
+// Routes init
+route(app)
 
 // 127.0.0.1 - localhost (nếu là localhost ==> phân giải DNS ==> 127.0.0.1)
 app.listen(port, () => {
